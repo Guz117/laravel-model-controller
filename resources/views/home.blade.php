1877fa3438157e93ef290d95c206b8e76cc3874b
Guz117 <cristian.guzman117@gmail.com>
2022-02-17 17:18:26 +0100

    scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,16 @@
 @extends('layout.base')
 
 @section('content')
+<h1>MOVIES</h1>
+<div class="cards">
     @foreach ($movies as $movie)
-       <div class="card">
-           <h2>{{ $movie->title }}</h2>
-           <h3>{{ $movie->original_title }}</h3>
-           <p>{{ $movie->nationality }}</p>
-           <p>{{ $movie->date }}</p>
-           <p>{{ $movie->vote }}</p>
-       </div>
+        <div class="card-movie">
+            <h2>{{ $movie->title }}</h2>
+            <h3>{{ $movie->original_title }}</h3>
+            <p>Nationality: <span>{{ $movie->nationality }}</span></p>
+            <p>{{  date_format(date_create($movie->date),'M d Y') }}</p>
+            <p>Vote: {{ $movie->vote }}</p>
+        </div>
     @endforeach
+</div>
 @endsection
